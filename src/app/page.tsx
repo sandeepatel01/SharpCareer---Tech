76f7c9c25f06f9c4d3471a2de7a5a5d5ad07569f
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-11-01 21:22:58 +0530

    Add Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <Header />
       <Navbar />
     </main>
   );
